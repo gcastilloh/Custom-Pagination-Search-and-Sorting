91938f4e789afd05ac524b6539233b8e2647217b
gcastilloh <gch@pumas.iingen.unam.mx>
2022-11-04 10:17:28 -0600

    first commit

---
+++ search/url.py
@@ -0,0 +1,10 @@
+
+from django.urls import path
+
+from .views import ProductFrontendAPIView, ProductBackendAPIView
+
+urlpatterns = [
+    path('products/frontend', ProductFrontendAPIView.as_view()),
+    path('products/backend', ProductBackendAPIView.as_view()),
+
+]
